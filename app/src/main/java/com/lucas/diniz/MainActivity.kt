280b83aea120b5512edceb22d1de024437e0129d
lucas-diniz <lukas-diniz@hotmail.com>
2022-01-06 21:15:55 -0300

    first commit

---
+++ app/src/main/java/com/lucas/diniz/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.lucas.diniz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lucas.diniz.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
